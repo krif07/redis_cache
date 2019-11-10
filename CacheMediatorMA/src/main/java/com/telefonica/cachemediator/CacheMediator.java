f006efff15859a4b3ee1826bdd66e8a62851a4fd
krif07 <krif07@gmail.com>
2019-11-09 22:18:22 -0500

    Mediador con caché

---
+++ CacheMediatorMA/src/main/java/com/telefonica/cachemediator/CacheMediator.java
@@ -0,0 +1,12 @@
+package com.telefonica.cachemediator;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class CacheMediator extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		return true;
+	}
+}
